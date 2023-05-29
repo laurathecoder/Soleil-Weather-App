8901faf8ee326d6e0bbb5c658e8c923481812531
laurathecoder <133053587+laurathecoder@users.noreply.github.com>
2023-05-29 19:28:02 +0200

    Updated to show current temperature, humidity & wind

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
-
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+let apiKey = "054c60e3eb320837b4fa1oe09b0bdt1a";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
